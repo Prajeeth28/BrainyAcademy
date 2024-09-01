42bc622383fac326337386e4de66c8ca6116b34e
Prajeeth28 <prajeeth06@gmail.com>
2024-09-01 21:12:43 +0530

    theme & plugin updated

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-108.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d3d39c07' );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-051faadc', {"layout":"number","heading":"Current Enrollments","numberPrefix":"","numberSuffix":"K+","startNumber":0,"endNumber":2,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-c41fef34', {"layout":"number","heading":"Qualified Staff","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-43f938f0', {"layout":"number","heading":"Clubs &amp; Activities","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-a92c0e67', {"layout":"number","heading":"Active PTFA Members","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"64b04035","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"abc@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-64b04035', 108 );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-62.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-15e76b7a', {"layout":"number","heading":"<strong><strong>Current Enrollments<\/strong><\/strong>","numberPrefix":"","numberSuffix":"K+","startNumber":0,"endNumber":2,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-34683923', {"layout":"number","heading":"<strong><strong><strong>Qualified Staff<\/strong><\/strong><\/strong>","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-7c544f57', {"layout":"number","heading":"<strong><strong>Clubs &amp; Activities<\/strong><\/strong>","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-eb74eb27', {"layout":"number","heading":"<strong><strong>Active PTFA Members<\/strong><\/strong>","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
